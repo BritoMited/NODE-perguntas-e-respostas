23037f7f5594d1b3134f985a6494ec707f7bee5e
BritoMited <gustavo.ponciano2005@outlook.com>
2025-11-09 12:46:05 -0300

    Organização e instalação do bootstrap

--- index.js
+++ index.js
@@ -1,56 +1,12 @@
-const express = require("express") // importação do express  
+const express = require("express") 
 const app = express()
 
-// configuração de rodar html 
-// dizendo para o express usar o EJS como view engine
-// para o express ler os arquivos html precisa estar na pasta views
-// e em extensão ejs
 app.set('view engine', 'ejs')
-
-// dizendo que será usado arquivos estaticos, como arquivos css ou imagens
-app.use(express.static('public')) // selecionando a pasta onde estarão esses arquivos, que por padrão é utilizado na pasta 'public'
-
-app.get("/:nome/:language", (req, res) => {
-
-    var nome = req.params.nome
-    var language = req.params.language
-
-    var exibirMsg = false 
-
-    var produtos = [ // criação de array
-        {nome: "Doritos", preco: 3.14},
-        {nome: "Coca-cola", preco: 5},
-        {nome: "Leite", preco: 1.14}
-    ]
-
-    // passagem de valores e variaveis para a pagina html
-    res.render("index", {
-        nome: nome,
-        language: language,
-        empresa: "HNSG",
-        ramal: 6681,
-        msg : exibirMsg,
-        produtos: produtos // passagem bem simples da array
-    })
-
-})
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
 
-    var nome = "britasso"
-    var language = "Java"
-
-    // passagem de valores e variaveis para a pagina html
-    res.render("index", {
-        nome: nome,
-        language: language,
-        empresa: "HNSG",
-        ramal: 6681
-    })
-
-    //res.render("principal/perfil")
-    //res.render("home")
-    //res.send("salve mano.")
+    res.render("index")
 
 })
 
